74423bbad789f551ad6615e27bab25ab54beb17e
jiyeon32 <kangjy003482@gmail.com>
2025-02-27 01:45:39 +0900

    feat: models/Chatroom.js추가

--- src/models/Chatroom.js
+++ src/models/Chatroom.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const chatRoomSchema = new mongoose.Schema({
     roomname: { type: String, required: true }
     },);
-  
+
 const ChatRoom = mongoose.model("ChatRoom", chatRoomSchema);
 export default ChatRoom;
