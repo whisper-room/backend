ebc1cd90094ff26cfbee31aa03c0293b64eaa945
hhyukk <hhyukk1273@gmail.com>
2025-02-28 17:28:22 +0900

    fix: 회원가입시 비밀번호 확인 기능 삭제

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,11 +2,8 @@ import User from '../models/User';
 import bcrypt from 'bcrypt';
 
 export const join = async (req, res) => {
-  const { username, email, password, password2 } = req.body;
+  const { username, email, password } = req.body;
 
-  if (password != password2) {
-    return res.status(400).json({ message: '비밀번호가 틀립니다.' });
-  }
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
     return res.status(400).json({ message: '닉네임이나 이메일이 이미 존재합니다.' });
