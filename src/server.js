4b420c74557c73f5a5b8d5c969cd82ee655896f6
hhyukk <hhyukk1273@gmail.com>
2025-02-28 19:58:22 +0900

    feat: cors 설정 추가

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 import './db.js';
 import express from 'express';
 import session from 'express-session';
+import cors from 'cors';
 import rootRouter from './routers/rootRouter.js';
 
 const app = express();
 
-app.use(session({
+app.use(
+  session({
     secret: 'myKey',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
-}));
+    cookie: { secure: false },
+  })
+);
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
