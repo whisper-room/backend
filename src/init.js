f86b6360b9066bccf8cc57cba7c188f800fcbe0d
hhyukk <hhyukk1273@gmail.com>
2025-03-20 14:57:58 +0900

    fix: Socket 수정 - Socket.IO 서버가 동작하도록 app.listen을 server.listen으로 변경

--- src/init.js
+++ src/init.js
@@ -1,9 +1,26 @@
 import 'dotenv/config';
 import './db.js';
+import http from 'http';
+import { Server } from 'socket.io';
 import app from './server';
+import socketHandlers from './socket.js';
 
 const PORT = 3000;
 
+// 1. HTTP 서버 생성
+const server = http.createServer(app);
+
+// 2. Socket.IO 서버 생성
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5000', // 프론트 주소
+    credentials: true,
+  },
+});
+
+// 3. 소켓 이벤트 등록
+socketHandlers(io);
+
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(PORT, handleListening);
+server.listen(PORT, handleListening);
---
+++ src/socket.js
@@ -0,0 +1,30 @@
+import Chat from './models/Chat';
+
+export default function socketHandlers(io) {
+  io.on('connection', (socket) => {
+    console.log('✅ 유저 연결됨:', socket.id);
+
+    socket.on('joinRoom', (roomId) => {
+      socket.join(roomId);
+      console.log(`${roomId} 채팅방에 입장했습니다.`);
+    });
+
+    socket.on('sendMessage', async ({ roomId, sender, text }) => {
+      console.log(`📩 메시지 도착 [방: ${roomId}], text`);
+
+      try {
+        const newMessage = new Chat({ roomId, sender, text });
+        await newMessage.save();
+
+        io.to(roomId).emit('receiveMessage', { sender, text });
+      } catch (error) {
+        console.error('🚨 메시지 저장 실패:', error);
+      }
+    });
+
+    socket.on('leaveRoom', (roomId) => {
+      socket.leave(roomId);
+      console.log(`🚪 유저가 ${roomId} 채팅방에서 나감`);
+    });
+  });
+}
